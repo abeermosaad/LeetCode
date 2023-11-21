97127bb4d7695c91797e8ff996a5dfb5c78246de
abeermosaad <abeermosaad69@gmail.com>
2023-11-21 17:25:14 +0200

    Stack

---
+++ 1598. Crawler Log Folder.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        for i in logs:
+            if i == '../':
+                if stack:
+                    stack.pop()
+            elif i == './':
+                continue
+            else:
+                stack.append(i)
+        return len(stack)
+        
